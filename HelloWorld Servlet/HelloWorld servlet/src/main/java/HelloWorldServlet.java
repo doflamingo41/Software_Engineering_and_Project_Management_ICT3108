22f5d04f508aa71b1bd34de111566c59b4199b24
Mahfuzur Rahman <it23005@mbstu.ac.bd>
2026-01-27 11:11:04 +0600

    Add HelloWorldServlet to handle GET requests

---
+++ HelloWorld Servlet/HelloWorld servlet/src/main/java/HelloWorldServlet.java
@@ -0,0 +1,14 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Hello World from Kuldip Saha Mugdha");
+    }
+}
